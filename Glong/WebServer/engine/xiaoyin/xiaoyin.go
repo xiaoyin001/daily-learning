681ea42ed759b88550a32c163af3457816de111f
xiaoyin <1>
2023-03-31 17:01:44 +0800

    [Update] Day Day Up (狗头) 这块内容就到这里，后面搞新的内容

--- Glong/WebServer/engine/xiaoyin/xiaoyin.go
+++ Glong/WebServer/engine/xiaoyin/xiaoyin.go
@@ -1,98 +1,99 @@
 package xiaoyin
 
 import (
-    "net/http"
-    "strings"
+	"net/http"
+	"strings"
 )
 
 // RouterGroup 路由组
 type RouterGroup struct {
-    basePath    string        // 当前组的URL前缀
-    middlewares []HandlerFunc // 中间件绑定方法
-    engine      *Engine       // 所有地方都用是一个
+	basePath    string        // 当前组的URL前缀
+	middlewares []HandlerFunc // 中间件绑定方法
+	engine      *Engine       // 所有地方都用的是一个
 }
 
 // AddRouteGroup 添加路由组
 func (group *RouterGroup) AddRouteGroup(aBasePath string) *RouterGroup {
-    mNewGroup := &RouterGroup{
-        basePath:    group.basePath + aBasePath,
-        middlewares: make([]HandlerFunc, 0),
-        engine:      group.engine,
-    }
-    
-    group.engine.AllGroups = append(group.engine.AllGroups, mNewGroup)
-    
-    return mNewGroup
+	mNewGroup := &RouterGroup{
+		basePath:    group.basePath + aBasePath,
+		middlewares: make([]HandlerFunc, 0),
+		engine:      group.engine,
+	}
+
+	group.engine.AllGroups = append(group.engine.AllGroups, mNewGroup)
+
+	return mNewGroup
 }
 
 // AddMiddlewares 添加中间件绑定的方法
 func (group *RouterGroup) AddMiddlewares(aMiddlewares ...HandlerFunc) {
-    group.middlewares = append(group.middlewares, aMiddlewares...)
+	group.middlewares = append(group.middlewares, aMiddlewares...)
 }
 
 // GET 添加Get请求路由
 func (group *RouterGroup) GET(aPath string, aHandler HandlerFunc) {
-    mPath := group.basePath + aPath
-    
-    group.engine.RouterMgr.addRouter("GET", mPath, aHandler)
+	mPath := group.basePath + aPath
+
+	group.engine.RouterMgr.addRouter("GET", mPath, aHandler)
 }
 
 // POST 添加Post请求路由
 func (group *RouterGroup) POST(aPath string, aHandler HandlerFunc) {
-    mPath := group.basePath + aPath
-    
-    group.engine.RouterMgr.addRouter("POST", mPath, aHandler)
+	mPath := group.basePath + aPath
+
+	group.engine.RouterMgr.addRouter("POST", mPath, aHandler)
 }
 
 /**********************************************************************************************************************/
 
 // Engine 实现HTTP服务接口的管理对象
 type Engine struct {
-    *RouterGroup                // 可以理解为继承 RouterGroup
-    AllGroups    []*RouterGroup // 所有的路由组
-    RouterMgr    *RouterMgr     // 路由管理
+	*RouterGroup                // 可以理解为继承 RouterGroup
+	AllGroups    []*RouterGroup // 所有的路由组
+	RouterMgr    *RouterMgr     // 路由管理
 }
 
 // Create 创建XiaoYinEngine服务管理对象
 func Create() *Engine {
-    mEngine := &Engine{
-        RouterGroup: &RouterGroup{},
-        AllGroups:   make([]*RouterGroup, 0),
-        RouterMgr:   NewRouter(),
-    }
-    
-    mEngine.middlewares = make([]HandlerFunc, 0)
-    mEngine.engine = mEngine
-    
-    return mEngine
+	mEngine := &Engine{
+		RouterGroup: &RouterGroup{},
+		AllGroups:   make([]*RouterGroup, 0),
+		RouterMgr:   NewRouter(),
+	}
+
+	mEngine.middlewares = make([]HandlerFunc, 0)
+	mEngine.engine = mEngine
+
+	return mEngine
 }
 
 // GET 添加Get请求路由
-func (e *Engine) GET(aPath string, aHandler HandlerFunc) {
-    e.RouterMgr.addRouter("GET", aPath, aHandler)
+func (engine *Engine) GET(aPath string, aHandler HandlerFunc) {
+	engine.RouterMgr.addRouter("GET", aPath, aHandler)
 }
 
 // POST 添加Post请求路由
-func (e *Engine) POST(aPath string, aHandler HandlerFunc) {
-    e.RouterMgr.addRouter("POST", aPath, aHandler)
+func (engine *Engine) POST(aPath string, aHandler HandlerFunc) {
+	engine.RouterMgr.addRouter("POST", aPath, aHandler)
 }
 
 // StartServer 启动服务
-func (e *Engine) StartServer(aAddr string) (err error) {
-    return http.ListenAndServe(aAddr, e)
+func (engine *Engine) StartServer(aAddr string) (err error) {
+	return http.ListenAndServe(aAddr, engine)
 }
 
 // 实现 ServeHTTP 方法，请求都会经过这里
-func (e *Engine) ServeHTTP(aRspW http.ResponseWriter, aReq *http.Request) {
-    mContext := NewContext(aRspW, aReq)
-    
-    // 先将匹配的中间件方法加入到切片中
-    for _, mGroup := range e.AllGroups {
-        if strings.HasPrefix(aReq.URL.Path, mGroup.basePath) {
-            mContext.Handlers = append(mContext.Handlers, mGroup.middlewares...)
-        }
-    }
-    
-    // 然后再执行路由方法
-    e.RouterMgr.ExecHandleFunc(mContext)
+func (engine *Engine) ServeHTTP(aRspW http.ResponseWriter, aReq *http.Request) {
+	mContext := NewContext(aRspW, aReq)
+
+	// 先将匹配的中间件方法加入到切片中
+	for _, mGroup := range engine.AllGroups {
+		// 检查 参数1 是否以 参数2 开头的
+		if strings.HasPrefix(aReq.URL.Path, mGroup.basePath) {
+			mContext.Handlers = append(mContext.Handlers, mGroup.middlewares...)
+		}
+	}
+
+	// 然后再执行路由方法
+	engine.RouterMgr.ExecHandleFunc(mContext)
 }
--- Glong/WebServer/main.go
+++ Glong/WebServer/main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-    "WebServer/engine/xiaoyin"
-    "fmt"
-    "net/http"
+	"WebServer/engine/xiaoyin"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    fmt.Println("哈哈哈，是时候开始了")
-    
-    mEngine := xiaoyin.Create()
-    
-    mGroup0 := mEngine.AddRouteGroup("/")
-    mGroup0.AddMiddlewares(testFunc02)
-    mGroup0.GET("/", testFunc01)
-    
-    mGroup1 := mEngine.AddRouteGroup("/xiaoyin01")
-    mGroup1.AddMiddlewares(testFunc02)
-    mGroup1.GET("/aa", testFunc01)
-    mGroup1.GET("/aa/bb", testFunc01)
-    
-    mGroup2 := mEngine.AddRouteGroup("/xiaoyin02")
-    mGroup2.AddMiddlewares(testFunc02)
-    mGroup2.GET("/aa", testFunc01)
-    mGroup2.GET("/aa/bb", testFunc01)
-    
-    err := mEngine.StartServer(":8888")
-    if err != nil {
-        fmt.Println("服务启动失败 --> Err:", err.Error())
-    }
+	fmt.Println("哈哈哈，是时候开始了")
+
+	mEngine := xiaoyin.Create()
+
+	mGroup0 := mEngine.AddRouteGroup("/")
+	mGroup0.AddMiddlewares(testFunc02)
+	mGroup0.GET("", testFunc01)
+
+	mGroup1 := mEngine.AddRouteGroup("/xiaoyin01")
+	mGroup1.AddMiddlewares(testFunc02)
+	mGroup1.GET("/aa", testFunc01)
+	mGroup1.GET("/aa/bb", testFunc01)
+
+	mGroup2 := mEngine.AddRouteGroup("/xiaoyin02")
+	mGroup2.AddMiddlewares(testFunc02)
+	mGroup2.GET("/aa", testFunc01)
+	mGroup2.GET("/aa/bb", testFunc01)
+
+	err := mEngine.StartServer(":8888")
+	if err != nil {
+		fmt.Println("服务启动失败 --> Err:", err.Error())
+	}
 }
 
 func testFunc01(aContext *xiaoyin.Context) {
-    mContent := fmt.Sprintf(" 测试返回内容 URL= %s \n", aContext.Path)
-    aContext.RspString(http.StatusOK, "%s"+mContent, "小印01")
-    
-    fmt.Printf("%s test路由的执行方法 URL: %s  Idx=%d \n", "小印01", aContext.Path, aContext.Idx)
+	mContent := fmt.Sprintf(" 测试返回内容 URL= %s \n", aContext.Path)
+	aContext.RspString(http.StatusOK, "%s"+mContent, "小印01")
+
+	fmt.Printf("%s test路由的执行方法 URL: %s  Idx=%d \n", "小印01", aContext.Path, aContext.Idx)
 }
 
 func testFunc02(aContext *xiaoyin.Context) {
-    fmt.Printf("%s test中间件的执行方法 URL: %s  Idx=%d \n", "小印02", aContext.Path, aContext.Idx)
+	fmt.Printf("%s test中间件的执行方法 URL: %s  Idx=%d \n", "小印02", aContext.Path, aContext.Idx)
 }
